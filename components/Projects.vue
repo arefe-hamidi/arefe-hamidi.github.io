663db904f3332cacd4c8eb74a178f42e31048482
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-08-03 13:05:02 +0330

    projects

--- components/Projects.vue
+++ components/Projects.vue
@@ -4,7 +4,22 @@
       <b-card-title outlined>
         <h4 class="title">Projects</h4>
       </b-card-title>
-      <b-card-text> </b-card-text>
+      <b-card-text>
+        <div v-for="(proj, index) in projects" :key="index">
+          <img
+            src="~/assets/icon/circle-solid.svg"
+            class="dot-icon mr-1"
+            alt=""
+          />
+          <a :href="proj.link">
+            <span class="title">{{ proj.title }}</span>
+          </a>
+
+          <span class="info-detail" v-if="proj.description"
+            >( {{ proj.description }} )</span
+          >
+        </div>
+      </b-card-text>
     </b-card>
   </div>
 </template>
@@ -12,7 +27,40 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      projects: [
+        {
+          title: "Taksan",
+          link: "https://taksaanpre.ir/",
+          description: " Iran's industrial market online platform",
+        },
+        {
+          title: "Taksan's academy site",
+          link: "https://academy.taksaan.ir/",
+          description: "",
+        },
+        {
+          title: "Taksan's advertisement site",
+          link: "https://agahi.taksaanpre.ir/",
+          description: "",
+        },
+        {
+          title: "Taksan's super admin panel",
+          link: "https://superadmin.taksaanpre.ir/",
+          description: "",
+        },
+        {
+          title: "Taksan's supplier panel",
+          link: "https://supplier.taksaanpre.ir/",
+          description: "",
+        },
+        {
+          title: "Memareno",
+          link: "https://memareno.com/",
+          description: "Home design and decoration guide",
+        },
+      ],
+    };
   },
 };
 </script>
